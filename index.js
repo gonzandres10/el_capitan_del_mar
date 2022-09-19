7c7c8b86b0cc881c569b91b72d6d096c75bcbcc9
Gonzalo <gonzandres9876@gmail.com>
2022-09-19 21:27:17 +0200

    Proyecto

---
+++ index.js
@@ -0,0 +1,80 @@
+
+
+function on() {
+    document.getElementById("menuoff").style.display = "block";
+    document.getElementById("menuon").style.display = "none";
+}
+
+function off() {
+    document.getElementById("menuon").style.display = "block";
+    document.getElementById("menuoff").style.display = "none";
+}
+
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+cookieContainer.classList.remove("active");
+localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+setTimeout(() => {
+if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+}
+}, 2000);
+
+const grande    = document.querySelector('.grande')
+const punto     = document.querySelectorAll('.punto')
+
+// Cuando CLICK en punto
+    // Saber la posición de ese punto
+    // Aplicar un transform translateX al grande
+    // QUITAR la clase activo de TODOS puntos
+    // AÑADIR la clase activo al punto que hemos hecho CLICK
+
+// Recorrer TODOS los punto
+punto.forEach( ( cadaPunto , i )=> {
+    // Asignamos un CLICK a cadaPunto
+    punto[i].addEventListener('click',()=>{
+
+        // Guardar la posición de ese PUNTO
+        let posicion  = i
+        // Calculando el espacio que debe DESPLAZARSE el GRANDE
+        let operacion = posicion * -60
+
+        // MOVEMOS el grand
+        grande.style.transform = `translateX(${ operacion }%)`
+
+        // Recorremos TODOS los punto
+        punto.forEach( ( cadaPunto , i )=>{
+            // Quitamos la clase ACTIVO a TODOS los punto
+            punto[i].classList.remove('activo')
+        })
+        // Añadir la clase activo en el punto que hemos hecho CLICK
+        punto[i].classList.add('activo')
+
+    })
+})
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+} else {
+    mybutton.style.display = "none";
+}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
+
+
